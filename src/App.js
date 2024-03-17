a700509fd526b6f0be3d0a9f8927060e59a9f38c
vanessacamargo1606 <vanessa.camargo@uao.edu.co>
2024-03-16 22:10:53 -0500

    Se actualizó App.js

--- src/App.js
+++ src/App.js
@@ -4,97 +4,88 @@ import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
-import {Board} from "./components/Board"
-import {ScoreBoard} from "./components/ScoreBoard"
+import { Board } from "./components/Board"
+import { ScoreBoard } from "./components/ScoreBoard"
 
-import {ResetButton} from "./components/ResetButton"
+import { ResetButton } from "./components/ResetButton"
 
 
 function App() {
 
   const WIN_CONDITIONS = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
   ]
 
   const [board, setBoard] = useState(Array(9).fill(null));
   const [xPlaying, setXPlaying] = useState(true);
-  const [scores, setScores] = useState({xScore: 0, oScore: 0})
+  const [scores, setScores] = useState({ xScore: 0, oScore: 0 })
 
   const [gameOver, setGameOver] = useState(false)
 
   const handleBoxClick = (boxIdx) => {
-    const updateBoard = board.map((value, idx) =>{
-      if(idx === boxIdx){
+    const updateBoard = board.map((value, idx) => {
+      if (idx === boxIdx) {
         return xPlaying === true ? "X" : "O";
-      }else{
-          return value;
-        }
-      
-
-    })
-
-    const winner = checkWinner(updateBoard);
-
-    if(winner){
-      if(winner === "0" ) {
-        let {oScore} = scores;
-        oScore +=1
-        setScores({...scores,oScore})
-      }else {
-        let {xScore} = scores;
-        xScore +=1
-        setScores({...scores,xScore})
-
+      } else {
+        return value;
       }
-    }
-
-    
+    })
 
     setBoard(updateBoard);
 
+    const winner = checkWinner(updateBoard);
+
+    if (winner) {
+      if (winner === "O") {
+        let { oScore } = scores;
+        oScore += 1;
+        setScores({ ...scores, oScore })
+      } else {
+        let { xScore } = scores;
+        xScore += 1;
+        setScores({ ...scores, xScore })
+      }
+    }
+
+
     setXPlaying(!xPlaying);
-  
+
 
   }
 
   const checkWinner = (board) => {
-    for (let i = 0; i < WIN_CONDITIONS.length; i++){
-      const [x,y,z] = WIN_CONDITIONS[i];
+    for (let i = 0; i < WIN_CONDITIONS.length; i++) {
+      const [x, y, z] = WIN_CONDITIONS[i];
 
-      if (board[x] && board[x] === board[y] & board[y] === board[z]){
+      if (board[x] && board[x] === board[y] & board[y] === board[z]) {
         setGameOver(true)
         return board[x];
       }
     }
   }
 
-  const resetBoard = () =>{
+  const resetBoard = () => {
     setGameOver(false);
     setBoard(Array(9).fill(null))
-    setScores({xScore: 0, oScore: 0})
-
-    
-    
-
+    //setScores({xScore: 0, oScore: 0})
   }
 
   return (
     <div className="App">
 
-      <ScoreBoard scores={scores} xPlaying={xPlaying}/>    
-      
-      <Board board={board} onClick={gameOver ? resetBoard : handleBoxClick}/>
+      <ScoreBoard scores={scores} xPlaying={xPlaying} />
 
-      <ResetButton resetBoard={resetBoard}/>
+      <Board board={board} onClick={gameOver ? resetBoard : handleBoxClick} />
 
-      
+      <ResetButton resetBoard={resetBoard} />
+
     </div>
   );
 }
